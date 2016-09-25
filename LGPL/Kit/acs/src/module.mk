1f40ae07ef094c4a8e507a01730d31f3e8fe1964
Tomas Staig <tstaig@csrg.inf.utfsm.cl>
2016-09-25 11:20:07 -0300

    Fixed scripts for man documentation

--- LGPL/Kit/acs/src/module.mk
+++ LGPL/Kit/acs/src/module.mk
@@ -23,8 +23,13 @@ INSTALL_FILES:= ../include/acsMakefile ../include/acsNewMakefile.mk \
       ../include/acsMakefileDefinitions.mk ../include/acsMakefileCore.mk \
       ../include/Makefile_LCU.template ../include/Makefile_WS.template \
       ../include/Makefile_PACKAGE.template $(wildcard ../config/acsPackageInfo*.rpmref) \
-		../config/XSDIncludeDependencies.xml
+      ../config/XSDIncludeDependencies.xml
 
+MANSECTIONS = 1 5 n
+MAN1 = acsMan acsConfigReport.py
+MAN5 = ../include/acsMakefile Makefile.doc
+MANn = acsReplace.tcl
+MANl = $(SCRIPTS)
 
 ###############
 ###############
@@ -36,33 +41,28 @@ INSTALL_FILES:= ../include/acsMakefile ../include/acsNewMakefile.mk \
 #PY_PACKAGES_L      =
 #pppppp_MODULES     =
 
-MANSECTIONS = 1 5 n
-MAN1 = acsMan acsConfigReport.py
-MAN5 = ../include/acsMakefile Makefile.doc
-MANn = acsReplace.tcl
-MANl = $(SCRIPTS)
-
 $(MODRULE)all: $(MODDEP) do_acsPort
-	$(AT)echo " . . . $(MODRULE)all done"
+	$(AT)echo " . . . $@ done"
 
-$(MODRULE)install: install_$(MODDEP)
-	$(AT)cp ../include/Makefile_PACKAGE.template \
-           ../include/Makefile_LCU.template \
-           ../include/Makefile_WS.template    $(PRJTOP)/include
-	$(AT)(cd $(PRJTOP)/include; rm -f vltPort.h; ln -s acsPort.h vltPort.h)
-	$(AT)echo " . . . $(MODRULE)install done"
+$(MODRULE)install: $(MODPATH) install_$(MODDEP) $(PRJTOP)/include/vltPort.h
+	$(AT)echo " . . . $@ done"
 
-$(MODRULE)clean: clean_$(MODDEP)
-	-$(AT) cp ../include/acsPort.h.DUMMY ../include/acsPort.h
-	$(AT)echo " . . . $(MODRULE)clean done"
+$(PRJTOP)/include/vltPort.h: $(PRJTOP)/include/acsPort.h
+	$(AT)rm -f $(PRJTOP)/include/vltPort.h
+	$(AT)ln -s $(PRJTOP)/include/acsPort.h $(PRJTOP)/include/vltPort.h
 
-$(MODRULE)clean_dist: clean_dist_$(MODDEP)
-	$(AT)echo " . . . $(MODRULE)clean_dist done"
+$(MODRULE)clean: $(MODPATH) clean_$(MODDEP)
+	-$(AT) cp $</include/acsPort.h.DUMMY $</include/acsPort.h
+	$(AT)echo " . . . $@ done"
+
+$(MODRULE)clean_dist: $(MODPATH) clean_dist_$(MODDEP)
+	$(AT)rm -f $(PRJTOP)/include/vltPort.h
+	$(AT)echo " . . . $@ done"
 
 man   : do_man
 	@echo " . . . man page(s) done"
 
-do_acsPort:
-	-$(AT) chmod 666   ../include/acsPort.h; cp ../include/acsPort.h.UNSUPPORTED ../include/acsPort.h; chmod 666   ../include/acsPort.h
-	-$(AT) if [ "`uname`" = "Linux" ]; then cp ../include/acsPort.h.Linux ../include/acsPort.h; chmod 666 ../include/acsPort.h; fi
-	-$(AT) if [ "`uname`" = "$(CYGWIN_VER)" ]; then cp ../include/acsPort.h.Cygwin ../include/acsPort.h; chmod 666 ../include/acsPort.h; fi
+do_acsPort: $(MODPATH)
+	-$(AT) chmod 666   $</include/acsPort.h; cp $</include/acsPort.h.UNSUPPORTED $</include/acsPort.h; chmod 666   $</include/acsPort.h
+	-$(AT) if [ "`uname`" = "Linux" ]; then cp $</include/acsPort.h.Linux $</include/acsPort.h; chmod 666 $</include/acsPort.h; fi
+	-$(AT) if [ "`uname`" = "$(CYGWIN_VER)" ]; then cp $</include/acsPort.h.Cygwin $</include/acsPort.h; chmod 666 $</include/acsPort.h; fi
